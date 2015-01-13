6af1849ec6f060fa8114858b3699a69c78449a05
AlexandreRivet <arivet@arivets-Mac.local>
2015-01-13 13:59:28 -0800

    Début du blackjack
    - Ajout de la classe carte
    - Ajout de la classe deck (reste à créer le système de défausse)
    - Ajout de la classe main

--- TestBlackjack/ViewController.m
+++ TestBlackjack/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "Card.h"
+#import "Deck.h"
+#import "Hand.h"
 
 @interface ViewController ()
 
@@ -14,8 +17,31 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    
+    // Création du tableau de cartes
+    NSMutableArray *cards = [[NSMutableArray alloc] init];
+    int numberDeck = 6;
+    for (int i = 0; i < numberDeck; i++)
+    {
+        for (int color = 0; color <= 3; color++)
+        {
+            for (int numberCard = 1; numberCard <= 13; numberCard++)
+            {
+                [cards addObject:[[Card alloc] initWithCardNumber:numberCard color:color]];
+            }
+        }
+    }
+    
+    // Deck* d = [[Deck alloc] initWithCards:cards];
+    // Hand* h = [[Hand alloc] init];
+    //Ajout de la 1ère carte
+    // [h addCard:[d drawCard]];
+    // Ajout de la 2ème carte
+    // [h addCard:[d drawCard]];
+    // NSLog([NSString stringWithFormat:@"%lu", (unsigned long)[h getValue]]);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
